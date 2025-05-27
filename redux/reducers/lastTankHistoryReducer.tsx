1e50b88ee93dd558c27d6d32f94ba9f3e26dd9f5
Vanecek0 <Vanecek0@seznam.cz>
2025-05-27 16:33:05 +0200

    redux

---
+++ redux/reducers/lastTankHistoryReducer.tsx
@@ -0,0 +1,12 @@
+import { SET_USER_NAME, SET_USER_AGE } from "../actions/lastTankHistoryAction"
+
+const initialState = {
+    name: "",
+    age: 0,
+}
+
+function reducer(state=initialState, action:any) {
+    switch(action.type) {
+
+    }
+}
