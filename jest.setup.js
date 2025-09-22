9716edb93a171659b3226d5dc69615cebba2720e
vanecek0 <vanecek0@seznam.cz>
2025-09-22 21:24:43 +0200

    Refactor Jest configuration and add tests for ScaledText component
    - Updated jest configuration in package.json to include new transformIgnorePatterns and setupFiles.
    - Created a mock for @react-native-async-storage/async-storage to facilitate testing.
    - Added comprehensive tests for ScaledText component, verifying text rendering, font size application, and line height scaling based on props.

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,3 @@
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
-
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
-
-jest.mock('expo-router', () => ({
-    Redirect: ({ href }) => <>{`Redirected to ${href}`}</>,
-}));
+jest.mock('@react-native-async-storage/async-storage', () =>
+    require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
