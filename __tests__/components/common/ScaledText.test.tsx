759b117ce63a013f0d68c3f63258e51a828ee888
vanecek0 <vanecek0@seznam.cz>
2025-09-22 21:41:21 +0200

    merge conflits

--- __tests__/components/common/ScaledText.test.tsx
+++ __tests__/components/common/ScaledText.test.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-<<<<<<< HEAD
 import { render } from '@testing-library/react-native';
-=======
-import { render } from "@testing-library/react-native";
->>>>>>> 8fe9e48 (Refactor Jest configuration and add tests for ScaledText component)
 import { ThemeProvider } from "@/theme/ThemeProvider";
 import ScaledText from "@/components/common/ScaledText";
 import { FontSizes } from "@/utils/fontScaling";
 import getScaleFactor, { getDeviceType } from "@/utils/SizeScaling";
 
-<<<<<<< HEAD
 jest.mock('@/utils/SizeScaling', () => ({
     __esModule: true,
     default: jest.fn(() => 2),
@@ -57,52 +52,3 @@ describe('<ScaledText />', () => {
         expect(textStyle.lineHeight).toBe(FontSizes["2xl"].lineHeight * 2);
     });
 })
-=======
-jest.mock("@/utils/SizeScaling", () => ({
-  __esModule: true,
-  default: jest.fn(() => 2),
-  getDeviceType: jest.fn(() => "phone-small"),
-}));
-
-describe("<ScaledText />", () => {
-  it("renders given text", async () => {
-    const { findByText } = render(
-      <ThemeProvider>
-        <ScaledText size="2xl">Testing the component</ScaledText>
-      </ThemeProvider>
-    );
-    await findByText("Testing the component");
-  });
-  it("applies correct font size and line height based on size prop", async () => {
-    const { findByText } = render(
-      <ThemeProvider>
-        <ScaledText size="2xl">Testing the component</ScaledText>
-      </ThemeProvider>
-    );
-
-    const text = await findByText("Testing the component");
-    expect(text.props.style).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          fontSize: FontSizes["2xl"].size,
-          lineHeight: FontSizes["2xl"].lineHeight * getScaleFactor(),
-        }),
-      ])
-    );
-  });
-  it("multiplies line height by scale factor", async () => {
-    const { findByText } = render(
-      <ThemeProvider>
-        <ScaledText size="2xl">Testing the component</ScaledText>
-      </ThemeProvider>
-    );
-
-    const textElement = await findByText("Testing the component");
-    const textStyle = Array.isArray(textElement.props.style)
-      ? Object.assign({}, ...textElement.props.style)
-      : textElement.props.style;
-
-    expect(textStyle.lineHeight).toBe(FontSizes["2xl"].lineHeight * 2);
-  });
-});
->>>>>>> 8fe9e48 (Refactor Jest configuration and add tests for ScaledText component)
