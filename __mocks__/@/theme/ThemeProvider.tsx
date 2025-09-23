e030d753556ba324f395dcc0e6d6547c5d1c582d
vanecek0 <vanecek0@seznam.cz>
2025-09-23 22:57:47 +0200

    remove testing files

--- __mocks__/@/theme/ThemeProvider.tsx
+++
@@ -1,4 +0,0 @@
-export const useTheme = jest.fn(() => ({ isDark: false }));
-const ThemeProvider = jest.fn(({ children }: any) => <>{children}</>);
-
-export default ThemeProvider;
--- __mocks__/@/utils/SizeScaling.js
+++
@@ -1,4 +0,0 @@
-export const getDeviceType = jest.fn(() => 'phone-small');
-const defaultScalingFactor = jest.fn(() => 2);
-
-export default defaultScalingFactor;
